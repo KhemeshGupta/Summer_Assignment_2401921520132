827b4784329f5b0c2f524144fa424c2f19733597
KhemeshGupta <guptakhemesh@gmail.com>
2026-06-04 12:21:17 +0530

    matrix diagonal sum

---
+++ Week_1/Day_4/matrix_diagonal_sum.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int diagonalSum(int[][] mat) {
+        int n=mat.length;
+        int ans=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(i==j || ((i+j)==(n-1))){
+                    ans+=mat[i][j];
+                }
+            }
+        }
+        return ans;
+    }
+}
